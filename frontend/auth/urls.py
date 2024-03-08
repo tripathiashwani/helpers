67e38127c6147a6e6505a9c589f19db114b852f8
ASHWANI TRIPATHI <tashwani475@gmail.com>
2024-03-08 22:41:18 +0530

    third

--- frontend/auth/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CustomerListView, HelperListView
-
-urlpatterns = [
-    path('customer/', CustomerListView.as_view(), name='customer-list'),
-    path('helper/', HelperListView.as_view(), name='helper-list'),
-]
